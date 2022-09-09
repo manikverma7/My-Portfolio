8a4e7b1a43375807a2bbe494fdcd30224dcc7e84
manikverma7 <manikverma6803@gmail.com>
2022-09-09 23:45:47 +0530

    Experience data in separate file

---
+++ src/data/experienceData.js
@@ -0,0 +1,41 @@
+import SchoolIcon from "@mui/icons-material/School";
+import WorkIcon from "@mui/icons-material/Work";
+
+export const experiences = [
+  {
+    title: "DAVIET, Jalandhar, Punjab",
+    subTitle: "Bachelor's Degree",
+    body: "Electronics & Communications",
+    icon: <SchoolIcon />,
+    date: "2015 - 2019",
+  },
+  {
+    title: "Content Writer",
+    subTitle: "Tutorzone, Jalandhar, Punjab",
+    body: "Wrote content in english to help Video Lecturers explain physics topics till 10th standard in video lectures.",
+    icon: <WorkIcon />,
+    date: "Mar 2019 - Aug 2020",
+  },
+
+  {
+    title: "Frontend Developer Trainee",
+    subTitle: "Nugen I.T. Services, Jalandhar, Punjab",
+    body: "Built a number of demo projects using Html, Css, Javascript and React.js",
+    icon: <WorkIcon />,
+    date: "Oct 2019 - Mar 2020",
+  },
+  {
+    title: "Home Tutor",
+    subTitle: "Star Tutorials, Jalandhar, Punjab",
+    body: "Taught Maths, Science and Coding to Students till 10th class.",
+    icon: <WorkIcon />,
+    date: "Nov 2019 - Aug 2020",
+  },
+  {
+    title: "Software Engineer (Frontend Developer)",
+    subTitle: "Benytsys, Mohali, Punjab",
+    body: "Built a number of Websites & Mobile Apps by working in following technologies : HTML5, Css3, Javascript, React, React-Native, Angular, Nest.js",
+    icon: <WorkIcon />,
+    date: "Aug 2020 - Present",
+  },
+];
